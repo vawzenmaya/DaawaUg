a3f12cfe701dd8d31263e1a6aacddb677af1753d
unknown <justuskayanja@gmail.com.com>
2024-05-30 07:22:31 +0100

    auth

--- lib/auth/register_finish_phone.dart
+++ lib/auth/register_finish_phone.dart
@@ -1,12 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 import 'package:toktok/auth/signin.dart';
 import 'package:toktok/navigation_container.dart';
 
+// ignore: must_be_immutable
 class RegisterFinishPhone extends StatelessWidget {
   final String phoneNumber;
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _contactController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  String? _password;
   final RxBool isObscure = true.obs;
+
+  Future<void> _signUp() async {
+    final response = await http.post(
+      Uri.parse("https://daawaug.000webhostapp.com/auth/signup_with_phone.php"),
+      body: {
+        'phoneNumber': _contactController.text,
+        'username': _usernameController.text,
+        'password': _passwordController.text,
+      },
+    );
+
+    String responseBody = response.body;
+    if (response.statusCode == 200) {
+      switch (responseBody) {
+        case 'username_already_taken':
+          Get.snackbar(
+            'Sorry username already taken',
+            'Please choose another username',
+            backgroundColor: Colors.red,
+            colorText: Colors.white,
+            snackPosition: SnackPosition.TOP,
+            duration: const Duration(seconds: 5),
+          );
+          break;
+        case 'registration_successful':
+          Get.offAll(() => const NavigationContainer());
+          Get.snackbar(
+            'Successfully',
+            'Signed up with Phone Number',
+            backgroundColor: Colors.grey,
+            colorText: Colors.white,
+            snackPosition: SnackPosition.TOP,
+            duration: const Duration(seconds: 5),
+          );
+          break;
+        case 'registration_failed':
+          Get.snackbar(
+            'Error',
+            'There was an error registering your account. Please try again.',
+            backgroundColor: Colors.red,
+            colorText: Colors.white,
+            snackPosition: SnackPosition.TOP,
+            duration: const Duration(seconds: 5),
+          );
+          break;
+        default:
+          Get.snackbar(
+            'Error',
+            'An unknown error occurred during registration',
+            backgroundColor: Colors.red,
+            colorText: Colors.white,
+            snackPosition: SnackPosition.TOP,
+            duration: const Duration(seconds: 5),
+          );
+      }
+    } else {
+      // Handle registration failure due to server error
+      Get.snackbar(
+        'Error',
+        'Failed to register. Please try again later.',
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.TOP,
+        duration: const Duration(seconds: 5),
+      );
+    }
+  }
 
   RegisterFinishPhone({super.key, required this.phoneNumber}) {
     _contactController.text = phoneNumber;
@@ -49,109 +122,139 @@ class RegisterFinishPhone extends StatelessWidget {
                   ),
                 ],
               ),
-              TextField(
-                controller: _contactController,
-                enabled: false,
-                decoration: const InputDecoration(
-                  labelText: 'Phone',
-                  labelStyle: TextStyle(color: Colors.grey),
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Colors.greenAccent, width: 2.5),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Full Real Names',
-                  labelStyle: TextStyle(color: Colors.grey),
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Colors.greenAccent, width: 2.5),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Obx(() => TextField(
-                    obscureText: isObscure.value,
-                    decoration: InputDecoration(
-                      labelText: 'Choose Password',
-                      labelStyle: const TextStyle(color: Colors.grey),
-                      border: const UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: const UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Colors.greenAccent, width: 2.5),
-                      ),
-                      suffixIcon: InkWell(
-                        onTap: () {
-                          isObscure.value = !isObscure.value;
-                        },
-                        child: Icon(
-                          isObscure.value
-                              ? Icons.visibility_off
-                              : Icons.visibility,
-                          color: isObscure.value
-                              ? Colors.grey
-                              : Colors.greenAccent,
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _contactController,
+                      enabled: false,
+                      decoration: const InputDecoration(
+                        labelText: 'Phone',
+                        labelStyle: TextStyle(color: Colors.grey),
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 2.5),
                         ),
                       ),
                     ),
-                  )),
-              const SizedBox(height: 10),
-              Obx(() => TextField(
-                    obscureText: isObscure.value,
-                    decoration: InputDecoration(
-                      labelText: 'Confirm Password',
-                      labelStyle: const TextStyle(color: Colors.grey),
-                      border: const UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: const UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Colors.greenAccent, width: 2.5),
-                      ),
-                      suffixIcon: InkWell(
-                        onTap: () {
-                          isObscure.value = !isObscure.value;
-                        },
-                        child: Icon(
-                          isObscure.value
-                              ? Icons.visibility_off
-                              : Icons.visibility,
-                          color: isObscure.value
-                              ? Colors.grey
-                              : Colors.greenAccent,
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: _usernameController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please choose your username';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        labelText: 'Choose a username',
+                        labelStyle: TextStyle(color: Colors.grey),
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.greenAccent, width: 2.5),
                         ),
                       ),
                     ),
-                  )),
-              const SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: () {
-                  Get.offAll(() => const NavigationContainer());
-                  Get.snackbar(
-                    'Successfully',
-                    'Signed up with Phone Number',
-                    backgroundColor: Colors.grey,
-                    colorText: Colors.white,
-                    snackPosition: SnackPosition.TOP,
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 50),
-                    backgroundColor: Colors.greenAccent),
-                child: const Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
+                    const SizedBox(height: 10),
+                    Obx(() => TextFormField(
+                          obscureText: isObscure.value,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please choose your password';
+                            }
+                            if (value.length < 8) {
+                              return 'Password must be at least 8 characters long';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            _password = value;
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Choose Password',
+                            labelStyle: const TextStyle(color: Colors.grey),
+                            border: const UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            focusedBorder: const UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.greenAccent, width: 2.5),
+                            ),
+                            suffixIcon: InkWell(
+                              onTap: () {
+                                isObscure.value = !isObscure.value;
+                              },
+                              child: Icon(
+                                isObscure.value
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color: isObscure.value
+                                    ? Colors.grey
+                                    : Colors.greenAccent,
+                              ),
+                            ),
+                          ),
+                        )),
+                    const SizedBox(height: 10),
+                    Obx(() => TextFormField(
+                          obscureText: isObscure.value,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please re-enter your password';
+                            }
+                            if (value != _password) {
+                              return 'Passwords must match';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Confirm Password',
+                            labelStyle: const TextStyle(color: Colors.grey),
+                            border: const UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            focusedBorder: const UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.greenAccent, width: 2.5),
+                            ),
+                            suffixIcon: InkWell(
+                              onTap: () {
+                                isObscure.value = !isObscure.value;
+                              },
+                              child: Icon(
+                                isObscure.value
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color: isObscure.value
+                                    ? Colors.grey
+                                    : Colors.greenAccent,
+                              ),
+                            ),
+                          ),
+                        )),
+                    const SizedBox(height: 50),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _signUp();
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 50),
+                          backgroundColor: Colors.greenAccent),
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               const SizedBox(height: 80),
